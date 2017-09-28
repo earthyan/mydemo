32d5ab2137458178205a8168b8ad2ec1bd7c2389
huhainan <huhainan@jianbing.com>
2017-09-28 14:21:23 +0800

    大鱼短信接口

--- phar.php
+++ phar.php
@@ -1,7 +1,7 @@
 <?php
 //首先需要修改php.ini配置将phar的readonly关闭，默认是不能写phar包的，include是默认开启的。
-//phar.readonly = Off
-$phar = new Phar('static.phar');
+//vendor.readonly = Off
+$phar = new Phar('static.vendor');
 $phar->buildFromDirectory(__DIR__.'/lib', '/\.php$/');
 $phar->compressFiles(Phar::GZ);
 $phar->stopBuffering();
